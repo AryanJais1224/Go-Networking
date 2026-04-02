94e0677165c2c08a31fa1330a17c8c3079278915
Aryann <129775380+AryanJais1224@users.noreply.github.com>
2026-04-02 22:41:01 +0530

    Create main.go

---
+++ tcp/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			return
+		}
+		fmt.Print("Client:", msg)
+		conn.Write([]byte("Received: " + msg))
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
